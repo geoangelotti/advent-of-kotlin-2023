fdee379f29240d6e9bd9224d83d890e8b1687888
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2023-12-14 18:40:45 +0100

    day 01 part1 change test

--- src/test/kotlin/day01/Day01Test.kt
+++ src/test/kotlin/day01/Day01Test.kt
@@ -4,13 +4,13 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day01Test {
-    val INPUT = """1abc2
+    private val input1 = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"""
 
     @Test
     fun `Test part 1`() {
-        assertEquals(Day01.processPart1(INPUT), 142)
+        assertEquals(142, Day01.processPart1(input1))
     }
 }
