9dfc85e956151728082891249a705bd925a74914
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2023-12-14 17:13:31 +0100

    solution

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -1,7 +1,21 @@
 package day02
 
 object Day02 {
-    fun processPart1(input: String): Int {
-        TODO()
-    }
+    fun processPart1(input: String): Int =
+        input.split("\n").sumOf { line ->
+            val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
+            val lineSplit = line.split(":")
+            val game = lineSplit[0].split(" ")[1].toInt()
+            val draws = lineSplit[1].split(";")
+            val impossibleGame = draws.map { draw ->
+                val cubes = draw.split(",")
+                val impossibleDraws = cubes.map{ cube->
+                    val (_, number, color) = cube.split(" ")
+                    val limit = limits.getOrDefault(color, 15)
+                    number.toInt() > limit
+                }
+                impossibleDraws.any{ it }
+            }.any{ it }
+            if (impossibleGame) 0 else game
+        }
 }
