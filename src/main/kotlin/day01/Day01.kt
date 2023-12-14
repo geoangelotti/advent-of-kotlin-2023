5b5054b38dc73d36440d9211ccf2a44e1b94a749
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2023-12-14 18:41:14 +0100

    day 01 part1 map, sum chain simplified

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -2,7 +2,7 @@ package day01
 
 object Day01 {
     fun processPart1(input:String) : Int =
-       input.split("\n").map{ processLine(it) }.sum()
+        input.split("\n").sumOf { processLine(it) }
 
     private fun processLine(input:String) : Int =
         input.filter { it.isDigit() }.let { it.first().digitToInt() * 10 + it.last().digitToInt() }
