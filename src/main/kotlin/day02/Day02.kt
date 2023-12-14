9a557d4fbd67db134ba4a54c47f2f26e111a140d
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2023-12-14 17:24:07 +0100

    rename line temporary value

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -4,10 +4,10 @@ object Day02 {
     private val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
     fun processPart1(input: String): Int =
-        input.split("\n").sumOf { line ->
-            val lineSplit = line.split(":")
-            val game = lineSplit[0].split(" ")[1].toInt()
-            val draws = lineSplit[1].split(";")
+        input.split("\n").sumOf {
+            val line = it.split(":")
+            val game = line[0].split(" ")[1].toInt()
+            val draws = line[1].split(";")
             if (draws.any { draw ->
                 draw.split(",").any{ cube->
                     val (_, number, color) = cube.split(" ")
