24b6faee94181b39ca943570261841b72895eaad
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2023-12-14 17:21:11 +0100

    mv limits to object

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -1,9 +1,10 @@
 package day02
 
 object Day02 {
+    private val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
+
     fun processPart1(input: String): Int =
         input.split("\n").sumOf { line ->
-            val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
             val lineSplit = line.split(":")
             val game = lineSplit[0].split(" ")[1].toInt()
             val draws = lineSplit[1].split(";")
