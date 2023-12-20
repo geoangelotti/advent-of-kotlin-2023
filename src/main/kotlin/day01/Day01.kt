a9c8e321c6f7b7ceb40148210115b55712aebb1e
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2023-12-20 17:29:13 +0100

    format code

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -1,9 +1,9 @@
 package day01
 
 object Day01 {
-    fun processPart1(input:String) : Int =
+    fun processPart1(input: String): Int =
         input.split("\n").sumOf { processLine(it) }
 
-    private fun processLine(input:String) : Int =
+    private fun processLine(input: String): Int =
         input.filter { it.isDigit() }.let { it.first().digitToInt() * 10 + it.last().digitToInt() }
 }
