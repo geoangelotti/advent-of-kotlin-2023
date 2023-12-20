127d04777295dce864e5ff2633e462d8fd26dc5b
Geo Angelopoulos <14923436+geoangelotti@users.noreply.github.com>
2023-12-20 19:59:47 +0100

    Day02/part1 (#2)
    * skeleton
    
    * test
    * binary run
    * object
    
    * day 02 input
    
    * assert proper spots
    
    * solution
    
    * map, any chain simplified
    
    * map, any chain simplified
    
    * mv limits to object
    
    * rename line temporary value
    
    * format code

---
+++ src/main/kotlin/day02/Day02.kt
@@ -0,0 +1,19 @@
+package day02
+
+object Day02 {
+    private val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
+
+    fun processPart1(input: String): Int =
+        input.split("\n").sumOf {
+            val line = it.split(":")
+            val game = line[0].split(" ")[1].toInt()
+            val draws = line[1].split(";")
+            if (draws.any { draw ->
+                    draw.split(",").any { cube ->
+                        val (_, number, color) = cube.split(" ")
+                        val limit = limits.getOrDefault(color, 15)
+                        number.toInt() > limit
+                    }
+                }) 0 else game
+        }
+}
