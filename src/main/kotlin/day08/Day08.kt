f40fda973780670083eb23f1074b0ad9bac60815
Geo Angelopoulos <14923436+geoangelotti@users.noreply.github.com>
2023-12-21 23:45:15 +0100

    Day08/part2 (#4)
    * extract duplicate code into a function
    
    * more modular check
    
    * format code
    
    * tail recursive solution
    
    * optimized solution
    
    * least common multiple
    
    * format code

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -1,19 +1,47 @@
 package day08
 
 object Day08 {
-    private tailrec fun recursivelyMove(node: Pair<String, String>, nodes: Map<String, Pair<String, String>>, iterator: MovesIterator, target: String, accumulator: Int): Int {
+    private tailrec fun recursivelyMove(
+        node: Pair<String, String>,
+        nodes: Map<String, Pair<String, String>>,
+        iterator: MovesIterator,
+        check: (String) -> Boolean,
+        accumulator: Int
+    ): Int {
         val next = accumulator + 1
         val nextNode = when (iterator.next()) {
             Move.Left -> node.first
             Move.Right -> node.second
         }
-        if (nextNode == target) {
+        if (check(nextNode)) {
             return next
         }
-        return recursivelyMove(nodes[nextNode]!!, nodes, iterator, target, next)
+        return recursivelyMove(nodes[nextNode]!!, nodes, iterator, check, next)
     }
 
-    fun processPart1(input: String): Int {
+    private tailrec fun recursivelySandMove(
+        nodes: List<Pair<String, String>>,
+        nodess: Map<String, Pair<String, String>>,
+        iterator: MovesIterator,
+        check: (String) -> Boolean,
+        accumulator: Int
+    ): Int {
+        val next = accumulator + 1
+        val move = iterator.next()
+        val nextNodes = nodes.map {
+            when (move) {
+                Move.Left -> it.first
+                Move.Right -> it.second
+            }
+        }
+        if (nextNodes.all { check(it) }) {
+            return next
+        }
+        return recursivelySandMove(nextNodes.map { nodess[it]!! }, nodess, iterator, check, next)
+    }
+
+
+    private fun generateInput(input: String, regex: Regex): Pair<List<Move>, Map<String, Pair<String, String>>> {
         val maps = input.filter { it != '\r' }.split("\n\n")
         val allowedChars = setOf('R', 'L')
         val moves = maps[0].filter { allowedChars.contains(it) }.split("").mapNotNull {
@@ -23,10 +51,41 @@ object Day08 {
                 else -> null
             }
         }
-        val regex = "([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)".toRegex()
         val nodes = maps[1].split("\n").mapNotNull { regex.find(it) }.associate {
             it.groupValues[1] to Pair(it.groupValues[2], it.groupValues[3])
         }
-        return recursivelyMove(nodes["AAA"]!!, nodes, MovesIterator(moves), "ZZZ", 0)
+        return Pair(moves, nodes)
+    }
+
+    fun processPart1(input: String): Int {
+        val regex = "([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)".toRegex()
+        val (moves, nodes) = generateInput(input, regex)
+        fun check(node: String): Boolean = node == "ZZZ"
+        return recursivelyMove(nodes["AAA"]!!, nodes, MovesIterator(moves), ::check, 0)
+    }
+
+    fun processPart2(input: String): Long {
+        val regex = "([A-Z0-9]{3}) = \\(([A-Z0-9]{3}), ([A-Z0-9]{3})\\)".toRegex()
+        val (moves, nodes) = generateInput(input, regex)
+        val starts = nodes.keys.filter { it.last() == 'A' }
+        fun check(node: String): Boolean = node.last() == 'Z'
+        return leastCommonMultiple(starts.map { recursivelyMove(nodes[it]!!, nodes, MovesIterator(moves), ::check, 0) })
+    }
+
+    private fun leastCommonMultiple(nums: List<Int>): Long {
+        if (nums.isEmpty())
+            return 1
+        val a = nums.first().toLong()
+        if (nums.size == 1)
+            return a
+        val b = leastCommonMultiple(nums.slice(1..<nums.size))
+        return a * b / greaterCommonDividor(a, b)
+    }
+
+    private fun greaterCommonDividor(a: Long, b: Long): Long {
+        if (b == (0).toLong()) {
+            return a
+        }
+        return greaterCommonDividor(b, a % b)
     }
 }
