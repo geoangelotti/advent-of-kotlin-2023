fc1e68c6ccfd02b480b5a58cc2ac91b3be23760d
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-01-17 13:00:51 +0100

    solution skeleton

---
+++ src/main/kotlin/day07/Day07.kt
@@ -0,0 +1,5 @@
+package day07
+
+object Day07 {
+    fun processPart1(input: String): Int = 0
+}
---
+++ src/test/kotlin/day07/Day07Test.kt
@@ -0,0 +1,17 @@
+package day07
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day07Test {
+    private val input = """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"""
+
+    @Test
+    fun `Test part 1`() {
+        assertEquals(6440, Day07.processPart1(input))
+    }
+}
