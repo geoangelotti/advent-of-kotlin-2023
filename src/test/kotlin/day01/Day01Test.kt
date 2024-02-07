21fdbe25d2119e461b9fd651082681b107d3e6b3
Geo Angelopoulos <14923436+geoangelotti@users.noreply.github.com>
2024-02-07 23:29:25 +0100

    Day01/part2 (#12)
    * solution
    
    * test

--- src/test/kotlin/day01/Day01Test.kt
+++ src/test/kotlin/day01/Day01Test.kt
@@ -9,8 +9,21 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"""
 
+    private val input2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
     @Test
     fun `Test part 1`() {
         assertEquals(142, Day01.processPart1(input1))
+    }
+
+    @Test
+    fun `Test part 2`() {
+        assertEquals(281, Day01.processPart2(input2))
     }
 }
