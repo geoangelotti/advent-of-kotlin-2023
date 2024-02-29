6db7a0129beebc58699ce812db315f26ef3506e8
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-02-29 17:37:41 +0100

    solution

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -14,4 +14,19 @@ object Day04 {
 
     fun processPart1(input: String): Int =
         processCards(input).sumOf { if (it == 0) 0 else 1 shl (it - 1) }
+
+    fun processPart2(input: String): Int {
+        val winners = processCards(input)
+        val listOfWinners = winners.map { mutableListOf(it) }
+        for (i in winners.indices) {
+            for (winner in listOfWinners[i]) {
+                for (j in i + 1..<i + winner + 1) {
+                    if (j < listOfWinners.size) {
+                        listOfWinners[j].add(winners[j])
+                    }
+                }
+            }
+        }
+        return listOfWinners.flatten().count()
+    }
 }
